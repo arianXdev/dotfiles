54e573e654ac30b759a567b357615f73c70a6d1b
Arian H <arianhosseini@outlook.com>
2023-12-28 15:02:17 +0330

    added dunst daemon | minor edits

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 lxsession --de="ARH" &
-picom &
+picom --daemon &
 alacritty -T cmus -e cmus &
 conky -c .conky-right & conky -c .conky-left & sleep 2s & nitrogen --restore & 
 clipmenud &
+dunst &
