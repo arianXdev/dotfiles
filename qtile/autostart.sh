d274d4aec0d900a4245651e82d403e2403f8751b
Arian H <arianhosseini@outlook.com>
2023-12-28 23:47:45 +0330

    A few changes

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,6 +2,11 @@
 lxsession --de="ARH" &
 picom --daemon &
 alacritty -T cmus -e cmus &
-conky -c .conky-right & conky -c .conky-left & sleep 2s & nitrogen --restore & 
+conky -c .conky-right & conky -c .conky-left & conky -c .conky-second-monitor & sleep 1s &
+nitrogen --restore & 
 clipmenud &
 dunst &
+
+
+
+#alacritty -T "terminal-floating" -o "window.dimensions.columns=40" -o "window.dimensions.lines=14" -o "window.position.y=400" -e COMMAND &
