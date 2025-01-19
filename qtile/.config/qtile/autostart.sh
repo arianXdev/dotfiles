4fe633fc76c7ab4df4aec74e9324ddaa17d99a75
Arian H. <arianhosseini@outlook.com>
2025-01-19 20:40:31 +0330

    feat: introducing some new changes

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -7,3 +7,7 @@ sleep 1 &
 nitrogen --restore & 
 conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center &
 conky -c ~/GitHub/my-conky-config/.conky-right & conky -c ~/GitHub/my-conky-config/.conky-left & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left &
+
+tmux new -d -s default &
+
+kdeconnectd &
--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -57,7 +57,7 @@ def maximize_by_switching_layout(qtile):
 # KEYS
 keys = [
     # The essentials
-    Key([mod], "Return", lazy.spawn(myTerm), desc="Launch terminal"),
+    Key([alt], "Return", lazy.spawn(myTerm), desc="Launch terminal"),
     Key([mod], 't', lazy.group['scratchpad'].dropdown_toggle('yazi')),
     Key([mod], "b", lazy.spawn(myBrowser), desc='Brave Browser'),
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
