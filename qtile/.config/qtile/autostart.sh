554b30233921403139961edb2273ee27fe104fcb
Arian H. <arianhosseini@outlook.com>
2025-01-23 18:24:15 +0330

    feat: Added my third conky config file

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -5,7 +5,7 @@ sxhkd &
 clipmenud &
 sleep 1 &
 nitrogen --restore & 
-conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center &
+conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/.conky-center &
 conky -c ~/GitHub/my-conky-config/.conky-right & conky -c ~/GitHub/my-conky-config/.conky-left & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left &
 
 tmux new -d -s default &
