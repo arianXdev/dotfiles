4a4c2db78a66fc6f97c7b6b989c5e3c78f73b85e
Arian H. <arianhosseini@outlook.com>
2025-02-01 23:42:27 +0330

    Update autostart.sh

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-dbus-update-activation-environment --all &
 picom --daemon &
 sxhkd &
-clipmenud &
-sleep 1 &
-nitrogen --restore & 
+nitrogen --restore &
+
 conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/.conky-center &
 conky -c ~/GitHub/my-conky-config/.conky-right & conky -c ~/GitHub/my-conky-config/.conky-left & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left &
 
+sleep 1 &
+
+clipmenud &
 tmux new -d -s default &
 
-kdeconnectd &
+kdeconnectd
