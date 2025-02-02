1575f4602d1801cd6db9545ebfd2a51617d43057
Arian H. <arianhosseini@outlook.com>
2025-02-02 21:03:41 +0330

    fix: fixed the conky paths

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -3,8 +3,8 @@ picom --daemon &
 sxhkd &
 nitrogen --restore &
 
-conky -c ~/GitHub/my-conky-config/.conky-center.lua & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center.lua & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/.conky-center.lua &
-conky -c ~/GitHub/my-conky-config/.conky-right.lua & conky -c ~/GitHub/my-conky-config/.conky-left.lua & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left.lua &
+conky -c ~/GitHub/my-conky-config/conky-center.lua & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/conky-center.lua & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/conky-center.lua &
+conky -c ~/GitHub/my-conky-config/conky-right.lua & conky -c ~/GitHub/my-conky-config/conky-left.lua & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/conky-left.lua &
 
 sleep 1 &
 
