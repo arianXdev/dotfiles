505331811f3526d2a47c198a423bd1c59af55c5f
Arian H. <arianhosseini@outlook.com>
2025-02-02 16:50:27 +0330

    fix: fixed the loading issue in autostart.sh

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -3,12 +3,12 @@ picom --daemon &
 sxhkd &
 nitrogen --restore &
 
-conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/.conky-center &
-conky -c ~/GitHub/my-conky-config/.conky-right & conky -c ~/GitHub/my-conky-config/.conky-left & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left &
+conky -c ~/GitHub/my-conky-config/.conky-center.lua & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-center.lua & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/.conky-center.lua &
+conky -c ~/GitHub/my-conky-config/.conky-right.lua & conky -c ~/GitHub/my-conky-config/.conky-left.lua & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/.conky-left.lua &
 
 sleep 1 &
 
 clipmenud &
 tmux new -d -s default &
 
-kdeconnectd
+kdeconnectd &
