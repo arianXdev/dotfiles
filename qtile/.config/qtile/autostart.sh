561428f44d83675f4def4b0400b801482530209a
Arian H. <arianhosseini@outlook.com>
2026-05-25 23:13:54 +0330

    chore: update package list, refine tmux UI, and replace nitrogen with waypaper

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 dbus-update-activation-environment --all &
 picom --daemon &
 sxhkd &
-nitrogen --restore &
+waypaper --restore &
 
 conky -c ~/GitHub/my-conky-config/conky-center.lua & CONKY_CENTER_CONFIG=2 conky -c ~/GitHub/my-conky-config/conky-center.lua & CONKY_CENTER_CONFIG=3 conky -c ~/GitHub/my-conky-config/conky-center.lua &
 conky -c ~/GitHub/my-conky-config/conky-right.lua & conky -c ~/GitHub/my-conky-config/conky-left.lua & CONKY_LEFT_CONFIG=2 conky -c ~/GitHub/my-conky-config/conky-left.lua &
